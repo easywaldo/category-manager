2056b2b799d70da42cc58305ff743e3e6a264204
easywaldoji <easywaldoji@gmail.com>
2021-12-22 00:04:09 +0900

    카테고리 제너레이터 실패하는 조회 테스트를 수정한다
    테스트 시작 전 초기데이터를 설정하도록 한다.

--- src/test/java/com/category/categorymanager/querygenerator/CategoryQueryGeneratorTest.java
+++ src/test/java/com/category/categorymanager/querygenerator/CategoryQueryGeneratorTest.java
@@ -2,6 +2,9 @@ package com.category.categorymanager.querygenerator;
 
 import com.category.categorymanager.category.command.QueryCategoryInfoCommand;
 import com.category.categorymanager.category.dto.CategoryInfoDto;
+import com.category.categorymanager.category.entity.CategoryInfo;
+import com.category.categorymanager.category.repository.CategoryInfoRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +20,35 @@ class CategoryQueryGeneratorTest {
 
     @Autowired
     public CategoryQueryGenerator queryGenerator;
+
+    @Autowired
+    public CategoryInfoRepository categoryInfoRepository;
+
+    @BeforeEach
+    public void setUp() {
+        categoryInfoRepository.deleteAll();
+        categoryInfoRepository.save(CategoryInfo.builder()
+            .parentSeq(0)
+            .categoryInfoSeq(1)
+            .categoryDepth(1)
+            .categoryName("Women")
+            .isDelete(false)
+            .build());
+        categoryInfoRepository.save(CategoryInfo.builder()
+            .parentSeq(1)
+            .categoryInfoSeq(2)
+            .categoryDepth(2)
+            .categoryName("Apparel")
+            .isDelete(false)
+            .build());
+        categoryInfoRepository.save(CategoryInfo.builder()
+            .parentSeq(2)
+            .categoryInfoSeq(3)
+            .categoryDepth(3)
+            .categoryName("Outer")
+            .isDelete(false)
+            .build());
+    }
 
     @Test
     public void 카테고리_데이터가_존재하는_경우_쿼리제너레이터가_카테고리_데이터를_리턴한다() {
