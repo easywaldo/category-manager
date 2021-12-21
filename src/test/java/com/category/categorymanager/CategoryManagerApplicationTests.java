1d23b7e540bda1410135dabe69a9310fb723eeae
easywaldo <easywaldoji@gmail.com>
2021-12-21 12:26:55 +0900

    FirstCommit

---
+++ src/test/java/com/category/categorymanager/CategoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.category.categorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
