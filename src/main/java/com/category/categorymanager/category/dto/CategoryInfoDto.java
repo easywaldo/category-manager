ec0cbf89660868b622971eb2d33d87b4d1535a67
easywaldo <easywaldoji@gmail.com>
2021-12-21 17:49:49 +0900

    카테고리 조회 QueryDSL Generator 추가

--- src/main/java/com/category/categorymanager/category/dto/CategoryInfoDto.java
+++ src/main/java/com/category/categorymanager/category/dto/CategoryInfoDto.java
@@ -1,2 +1,47 @@
-package com.category.categorymanager.category.dto;public class CategoryInfoDto {
+package com.category.categorymanager.category.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CategoryInfoDto {
+
+    private Integer categoryInfoSeq;
+    private String categoryName;
+    private Integer categoryDepth;
+    private Integer parentSeq;
+
+    private Integer depth2CategoryInfoSeq;
+    private String depth2CategoryName;
+    private Integer depth2ParentSeq;
+
+    private Integer depth3CategoryInfoSeq;
+    private String depth3CategoryName;
+    private Integer depth3ParentSeq;
+
+
+    @Builder
+    public CategoryInfoDto(Integer categoryInfoSeq,
+                           String categoryName,
+                           Integer categoryDepth,
+                           Integer parentSeq,
+                           Integer depth2CategoryInfoSeq,
+                           String depth2CategoryName,
+                           Integer depth2ParentSeq,
+                           Integer depth3CategoryInfoSeq,
+                           String depth3CategoryName,
+                           Integer depth3ParentSeq) {
+        this.categoryInfoSeq = categoryInfoSeq;
+        this.categoryName = categoryName;
+        this.categoryDepth = categoryDepth;
+        this.parentSeq = parentSeq;
+        this.depth2CategoryInfoSeq = depth2CategoryInfoSeq;
+        this.depth2CategoryName = depth2CategoryName;
+        this.depth2ParentSeq = depth2ParentSeq;
+        this.depth3CategoryInfoSeq = depth3CategoryInfoSeq;
+        this.depth3CategoryName = depth3CategoryName;
+        this.depth3ParentSeq = depth3ParentSeq;
+    }
 }
--- src/main/java/com/category/categorymanager/querygenerator/CategoryQueryGenerator.java
+++ src/main/java/com/category/categorymanager/querygenerator/CategoryQueryGenerator.java
@@ -1,2 +1,62 @@
-package com.category.categorymanager.querygenerator;public class CategoryQueryGenerator {
+package com.category.categorymanager.querygenerator;
+
+import com.category.categorymanager.category.command.QueryCategoryInfoCommand;
+import com.category.categorymanager.category.dto.CategoryInfoDto;
+import com.category.categorymanager.category.entity.QCategoryInfo;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static com.category.categorymanager.category.entity.QCategoryInfo.categoryInfo;
+
+@Repository(value = "category")
+public class CategoryQueryGenerator {
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+    protected JPAQueryFactory jpaQueryFactory;
+
+    public CategoryQueryGenerator() {
+        this.jpaQueryFactory = new JPAQueryFactory(this.entityManager);
+    }
+
+    public List<CategoryInfoDto> selectCategoryInfo(QueryCategoryInfoCommand command) {
+        QCategoryInfo categoryInfoDepth2 = new QCategoryInfo("qci2");
+        QCategoryInfo categoryInfoDepth3 = new QCategoryInfo("qci3");
+
+        BooleanBuilder whereClause = new BooleanBuilder();
+        if (!Strings.isNotBlank(command.getCategoryName())) {
+            whereClause.or(categoryInfo.categoryName.like("%" + command.getCategoryName() + "%"));
+            whereClause.or(categoryInfoDepth2.categoryName.like("%" + command.getCategoryName() + "%"));
+            whereClause.or(categoryInfoDepth3.categoryName.like("%" + command.getCategoryName() + "%"));
+        }
+
+        if (command.getCategoryParentSeq() > 0) {
+            whereClause.or(categoryInfo.parentSeq.eq(command.getCategoryParentSeq()));
+            whereClause.or(categoryInfoDepth2.parentSeq.eq(command.getCategoryParentSeq()));
+            whereClause.or(categoryInfoDepth3.parentSeq.eq(command.getCategoryParentSeq()));
+        }
+
+        return this.jpaQueryFactory.from(categoryInfo)
+            .leftJoin(categoryInfoDepth2).on(categoryInfoDepth2.parentSeq.eq(categoryInfo.categoryInfoSeq))
+            .leftJoin(categoryInfoDepth3).on(categoryInfoDepth3.parentSeq.eq(categoryInfo.categoryInfoSeq))
+            .where(whereClause)
+            .select(Projections.fields(CategoryInfoDto.class,
+                categoryInfo.categoryInfoSeq,
+                categoryInfo.categoryName,
+                categoryInfoDepth2.categoryInfoSeq.as("depth2CategoryInfoSeq"),
+                categoryInfoDepth2.categoryName.as("depth2CategoryName"),
+                categoryInfoDepth2.parentSeq.as("depth2ParentSeq"),
+                categoryInfoDepth3.categoryInfoSeq.as("depth3CategoryInfoSeq"),
+                categoryInfoDepth3.categoryName.as("depth3CategoryName"),
+                categoryInfoDepth3.parentSeq.as("depth3ParentSeq")))
+            .fetch();
+    }
+
 }
